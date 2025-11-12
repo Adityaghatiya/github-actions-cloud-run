937ca1509c927d503478b8323416679fc99e1761
Adityaghatiya <ghatiyaaditya@gmail.com>
2025-11-12 21:57:54 +0530

    app.py added11

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello from Cloud Run — Flask app is working!"
+    return "✅ Cloud Run Flask App Working!"
 
 if __name__ == '__main__':
-    # Cloud Run sets the PORT environment variable automatically
     port = int(os.environ.get("PORT", 8080))
-    # ✅ Must listen on 0.0.0.0 — NOT localhost
     app.run(host="0.0.0.0", port=port)
